5fae9f203023a5cfea42a43c254c639b462b39a5
huynhnguyen <nguyenhuynh200188@gmail.com>
2019-01-07 15:22:28 +0700

    [dataset] update mnist

--- datasets/splittingMNISTDataset.babel.node.js
+++ datasets/splittingMNISTDataset.babel.node.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bed965571bf2fea40e1bd2cf50ed390396246df6930933f87bab6c8d983a9cb
-size 2254
+oid sha256:5b5a570c9882dbdfb194b7016c67c0213a91d01080404d1cf646fca03388db59
+size 2344
