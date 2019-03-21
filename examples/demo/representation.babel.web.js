1aca61412c6e62e7da7f33745b13ea8f2e138a97
Nguyen Huynh <nguyenhuynh200188@gmail.com>
2019-03-21 16:03:43 +0700

    [wip] on demo

--- examples/demo/representation.babel.web.js
+++ examples/demo/representation.babel.web.js
@@ -4,8 +4,14 @@ import { universalEmbedding } from 'causal-net.representation';
     // termLogger.groupBegin();
     await universalEmbedding.connect();
     // termLogger.groupEnd();
-    let sentVec = await universalEmbedding.sentenceEncode([ 'this is test' ]);
-    sentVec.print();
-    let diffScore = await universalEmbedding.encodeMatching( 'this is a good day' , 'this is a terrible day' );
-    diffScore.print();  
+    // let sentVec = await universalEmbedding.sentenceEncode([ 'this is test' ]);
+    // sentVec.print();
+    // let diffScore = await universalEmbedding.encodeMatching( 'this is a good day' , 'this is a terrible day' );
+    // diffScore.print();  
 })();
+$('#compare-text').click(function(){
+
+});
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
